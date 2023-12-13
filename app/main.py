3749403feb3cf3c93f77634fc0443bb8b81c1bc3
Seongha <hosh2326@gmail.com>
2023-12-13 18:04:35 +0900

    big wave

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ def info():
     with connection.cursor() as cursor:
         # 쿼리 실행
         cursor.execute(
-            "SELECT * FROM `subway`.`subway` WHERE `업종` = '화장품(미용)' ORDER BY `위치` ASC LIMIT 300 OFFSET 0;")
+            "SELECT * FROM `subway`.`subway` ORDER BY CASE `업종` WHEN '의류(옷/속옷)' THEN 1 WHEN '신발(잡화)' THEN 2 WHEN '보석' THEN 3 WHEN '화장품(미용)' THEN 4 WHEN '수선' THEN 5 WHEN '핸드폰' THEN 6 WHEN '기타' THEN 7 ELSE 8 END ASC,`위치` ASC LIMIT 300 OFFSET 0;")
 
         data = cursor.fetchall()  # 가져온 데이터를 변수에 저장
     # HTML 템플릿에 데이터 전달
