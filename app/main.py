4f66fc0448b47086ba41e518f564228e7fd8eae1
Seongha Kim <hosh2326@naver.com>
2023-12-13 09:45:32 +0900

    info : fetchone

--- app/main.py
+++ app/main.py
@@ -19,8 +19,9 @@ def index():
 def cos():
     with connection.cursor() as cursor:
         # 쿼리 실행
-        cursor.execute("SELECT * FROM `subway`.`subway` WHERE `업종` = '화장품(미용)' ORDER BY `위치` ASC LIMIT 300 OFFSET 0;")
-        
+        cursor.execute(
+            "SELECT * FROM `subway`.`subway` WHERE `업종` = '화장품(미용)' ORDER BY `위치` ASC LIMIT 300 OFFSET 0;")
+
         data = cursor.fetchall()  # 가져온 데이터를 변수에 저장
     # HTML 템플릿에 데이터 전달
     return render_template('cos.html', data=data)
@@ -28,7 +29,14 @@ def cos():
 
 @app.route('/info')
 def info():
-    return render_template('info.html')
+    with connection.cursor() as cursor:
+        # 쿼리 실행
+        cursor.execute(
+            "SELECT * FROM `subway`.`subway` WHERE `업종` = '화장품(미용)' ORDER BY `위치` ASC LIMIT 300 OFFSET 0;")
+
+        data = cursor.fetchone()  # 가져온 데이터를 변수에 저장
+    # HTML 템플릿에 데이터 전달
+    return render_template('info.html', data=data)
 
 
 if __name__ == '__main__':
